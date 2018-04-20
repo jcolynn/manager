95eda945fbe4ef709c07bf4a3315d289caf7dbb3
Cody Lynn <jclynn87@gmail.com>
2018-04-19 21:48:23 -0400

    Implement types.js and update AuthReducer

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
+//File to export variable to actions and reducer files
+
 export const EMAIL_CHANGED = "email_changed";
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,12 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
+//Variable for state
 const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
     default:
       return state;
   }
